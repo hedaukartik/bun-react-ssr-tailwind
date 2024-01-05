83cc0da96f3c74d26a7eb462e05d8a0f36c4d8e7
khedau <khedau@zscaler.com>
2024-01-05 20:06:53 +0530

    remove missed console

--- hydrate.tsx
+++ hydrate.tsx
@@ -15,6 +15,4 @@ declare global {
 import { hydrateRoot } from "react-dom/client";
 const { default: App } = await import(globalThis.PATH_TO_PAGE);
 
-console.log(globalThis.props);
-
 hydrateRoot(document, <App {...globalThis.props} />);
