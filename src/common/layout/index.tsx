5965572f439869dac0d4f153077b46598874d561
khedau <khedau@zscaler.com>
2024-01-05 21:49:04 +0530

    serve public folder for assets

--- src/common/layout/index.tsx
+++ src/common/layout/index.tsx
@@ -5,7 +5,7 @@ export function Layout(props: { title: string; children: React.ReactNode }) {
     <html>
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
